a468da0f82867624dd911026aac474d8e65a3b7d
Toby Usher <tobylerone5@gmail.com>
2025-04-15 21:51:32 +0100

    small formatting changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = [
     "192.168.0.22",
     "10.159.139.194",
     "192.168.202.50",
-    "192.168.10.114" # train
+    "192.168.0.14" # train
     ]
 
 CORS_ALLOWED_ORIGINS = [f"http://{host}" for host in ALLOWED_HOSTS]
